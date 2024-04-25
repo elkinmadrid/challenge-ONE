a7920e36c41a629fdcd84866e081f7b964b6a63f
elkinmadrid <elkinamp@gmail.com>
2024-04-24 22:29:02 -0500

    Challenge completed

---
+++ README.md
@@ -0,0 +1,31 @@
+# Convertidor de Monedas
+
+Este es un proyecto de convertidor de monedas desarrollado en Java que permite realizar conversiones entre diferentes monedas utilizando tasas de cambio proporcionadas. El programa proporciona un menú interactivo en la consola para seleccionar las conversiones deseadas.
+
+## Funcionalidades
+
+- Conversión de Dólar a Peso argentino y viceversa.
+- Conversión de Dólar a Real brasileño y viceversa.
+- Conversión de Dólar a Peso Colombiano y viceversa.
+- Interfaz de usuario interactiva en la consola para seleccionar las conversiones.
+- Utiliza tasas de cambio proporcionadas para realizar las conversiones.
+
+## Tecnologías Utilizadas
+
+- Java 17
+- HttpClient para realizar la petición a la API 
+- Scanner para la entrada de usuario desde la consola
+
+## Instrucciones de Uso
+
+1. Clona el repositorio a tu máquina local.
+2. Abre el proyecto en tu entorno de desarrollo preferido (Eclipse, IntelliJ, etc.).
+3. Ejecuta el comando `gradle clean build`
+3. Ejecuta la clase `Main` para iniciar el programa.
+4. Sigue las instrucciones en la consola para realizar las conversiones deseadas.
+5. Para salir del programa, selecciona la opción "Salir" en el menú.
+
+## Autor
+
+- Nombre: Elkin Madrid
+- Contacto: [Tu correo electrónico]
--- src/main/java/co/elkinmadrid/Main.java
+++ src/main/java/co/elkinmadrid/Main.java
@@ -1,7 +1,105 @@
 package co.elkinmadrid;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        JsonObject conversion_rates = requestAPI();
+        int option;
+        do {
+            System.out.println("""
+                    *********************************************
+                    *********************************************
+                                        
+                    1- Dolar => Peso argentino
+                    2- Peso argentino => Dolar
+                    3- Dolar => Real brasileño
+                    4- Real brasileño => Dolar
+                    5- Dolar => Peso Colombiano
+                    6- Peso Colombiano => Dolar
+                    7- Salir
+                                        
+                    Digite una opción valida:
+                    *********************************************
+                    *********************************************
+                    """);
+
+            Scanner scanner = new Scanner(System.in);
+            option = scanner.nextInt();
+            double amount = 0;
+            if (option >= 1 && option <= 6) {
+                System.out.println("Ingrese la cantidad que desea convertir: ");
+                amount = scanner.nextDouble();
+            }
+
+            double currencyExchange = switch (option) {
+
+                case 1 -> {
+                    System.out.println("Dolar a peso argentino");
+                    yield converterCurrency("USD", "ARS", conversion_rates, amount);
+                }
+                case 2 -> {
+                    System.out.println("Peso argentino a Dolar");
+                    yield converterCurrency("ARS", "USD", conversion_rates, amount);
+                }
+                case 3 -> {
+                    System.out.println("Dolar a real brasileño");
+                    yield converterCurrency("USD", "BRL", conversion_rates, amount);
+                }
+                case 4 -> {
+                    System.out.println("Real brasileño a Dolar");
+                    yield converterCurrency("BRL", "USD", conversion_rates, amount);
+                }
+                case 5 -> {
+                    System.out.println("Dolar a Peso Colombiano");
+                    yield converterCurrency("USD", "COP", conversion_rates, amount);
+                }
+                case 6 -> {
+                    System.out.println("Peso Colombiano a Dolar");
+                    yield converterCurrency("COP", "USD", conversion_rates, amount);
+                }
+                case 7 -> {
+                    System.out.println("Saliendo del programa");
+                    yield 0;
+                }
+                default -> {
+                    System.out.println("Opción inválida. Por favor, seleccione una opción válida.");
+                    yield 0;
+                }
+            };
+            if (option != 7) {
+                System.out.println("El valor de " + amount + " corresponde al valor de => " + currencyExchange);
+            }
+        } while (option != 7);
+
+
+    }
+
+    static double converterCurrency(String to, String from, JsonObject conversion_rates, double amount) {
+        double tasaOrigen = conversion_rates.get(to).getAsDouble();
+        double tasaDestino = conversion_rates.get(from).getAsDouble();
+        double cantidadEnOrigen = amount / tasaOrigen;
+        return cantidadEnOrigen * tasaDestino;
+    }
+
+    static JsonObject requestAPI() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://v6.exchangerate-api.com/v6/fffa750de38bc301f2675101/latest/USD"))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JsonObject responseParse = JsonParser.parseString(response.body()).getAsJsonObject();
+        return responseParse.get("conversion_rates").getAsJsonObject();
     }
 }
